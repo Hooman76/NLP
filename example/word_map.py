aeb2326dd0f5c382361a173793ae496c77de670e
Hooman Mehrafarin <Hooman76>
2018-03-20 13:17:20 +0330

    second task added (word map)

---
+++ example/word_map.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Minimal Example
+===============
+
+Generating a square wordcloud from the US constitution using default arguments.
+"""
+from os import path
+
+from persian_wordcloud.wordcloud import PersianWordCloud, add_stop_words
+
+d = path.dirname(__file__)
+
+text = open(path.join(d, 'sohrab.txt'), encoding='utf-8').read()
+
+# Add another stopword
+stopwords = add_stop_words(['شاسوسا'])
+# add_stop_words
+
+# Generate a word cloud image
+wordcloud = PersianWordCloud(
+    only_persian=True,
+    max_words=100,
+    stopwords=stopwords,
+    margin=0,
+    width=800,
+    height=800,
+    min_font_size=1,
+    max_font_size=500,
+    background_color="black"
+).generate(text)
+
+image = wordcloud.to_image()
+image.show()
+image.save('sepehri_word_map.png')
